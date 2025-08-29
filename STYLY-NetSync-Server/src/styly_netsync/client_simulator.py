50066b06839e0b12a90c85787596fc2f7e836406
Masahiro Yamaguchi <from2001@hotmail.com>
2025-08-29 12:56:54 +0900

    Fix: Missing constants

--- STYLY-NetSync-Server/src/styly_netsync/client_simulator.py
+++ STYLY-NetSync-Server/src/styly_netsync/client_simulator.py
@@ -519,6 +519,9 @@ class SimulatedClient:
     """Represents a single simulated client."""
     
     # Battery simulation constants
+    BATTERY_INITIAL_MIN = 50.0        # %
+    BATTERY_INITIAL_MAX = 100.0       # %
+    BATTERY_DRAIN_RATE = 10.0 / 60.0  # %-points lost per second (10%/min)
     BATTERY_UPDATE_INTERVAL = 60.0     # Send battery updates every 60 seconds
     
     def __init__(
