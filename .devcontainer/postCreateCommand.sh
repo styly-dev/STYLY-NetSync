ef7c7fe1217c3637b475b300d2e3b00bc4f91d6e
Masahiro Yamaguchi <from2001@hotmail.com>
2025-11-12 06:03:26 +0000

    chore: update postCreateCommand.sh to include pip upgrade and development dependencies installation

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,6 +2,11 @@
 
 cd STYLY-NetSync-Server/
 
+# Upgrade pip to avoid version warnings
+pip install --upgrade pip
+
+# Install development dependencies
+pip install -e ".[dev]"
+
 # Install in development mode
 pip install -e .
-
