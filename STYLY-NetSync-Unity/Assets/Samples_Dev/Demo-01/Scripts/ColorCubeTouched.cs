87d6e92da4344d39374863e57c533aa73debd8e8
Masahiro Yamaguchi <from2001@hotmail.com>
2026-01-22 11:53:54 -0500

    Implement Touch count

--- STYLY-NetSync-Unity/Assets/Samples_Dev/Demo-01/Scripts/ColorCubeTouched.cs
+++ STYLY-NetSync-Unity/Assets/Samples_Dev/Demo-01/Scripts/ColorCubeTouched.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using Styly.NetSync;
 
+[RequireComponent(typeof(Renderer))]
 public class ColorCubeTouched : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
@@ -17,5 +18,21 @@ public class ColorCubeTouched : MonoBehaviour
 
         // Set the client variable for the touched color
         NetSyncManager.Instance.SetClientVariable("Touched Color", color.ToString());
+        IncrementTouchCount_Global();
+        IncrementTouchCount_Client();
+    }
+
+    void IncrementTouchCount_Global()
+    {
+        int touchCount = int.Parse(NetSyncManager.Instance.GetGlobalVariable("Total Touch Count", "0"));
+        touchCount++;
+        NetSyncManager.Instance.SetGlobalVariable("Total Touch Count", touchCount.ToString());
+    }
+
+    void IncrementTouchCount_Client()
+    {
+        int touchCount = int.Parse(NetSyncManager.Instance.GetClientVariable("Touch Count", "0"));
+        touchCount++;
+        NetSyncManager.Instance.SetClientVariable("Touch Count", touchCount.ToString());
     }
 }
